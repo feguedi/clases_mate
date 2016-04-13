c80b2948369157dbca350d6c14b488f954fc14e5
Eduardo Fernández <feguedi@yahoo.com.mx>
2016-04-13 13:27:48 -0500

    Agregando clase de 2/marzo

---
+++ ProbabilidadEstadistica/Ejercicios/ejercicio11Abril.py
@@ -0,0 +1,5 @@
+import math
+
+listilla = {(0, 0, 0, 0),
+            ()
+}
---
+++ ProbabilidadEstadistica/Ejercicios/juseff.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import math
+
+__author__ = 'nger'
+"""Eventos pendientes y dependientes a partir de la cardinalidad de ciertos valores"""
+
+if __name__ == '__main__':
+    print(math.pi)
