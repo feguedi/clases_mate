3da998fb048c568b1e31f88f34c7349f33b3a260
Eduardo Fernández <feguedi@yahoo.com.mx>
2016-05-02 15:29:07 -0500

    Cambios en el main del CFEmatico

--- Paradigmas/cfematico/main.py
+++ Paradigmas/cfematico/main.py
@@ -5,11 +5,5 @@ import csv
 
 comprobante = 'texto_comprobante.csv'
 
-
-def importar(comprobante):
-    attendee_data = csv.reader(open(comprobante, 'rb'))
-    for fila in attendee_data:
-        apellido = fila[0]
-
 if __name__ == '__main__':
     print('ola')
